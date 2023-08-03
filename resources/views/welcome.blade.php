dbb0bd720452df409f479b5ddaafe9ad12770622
malilfajr <malilfajar17@gmail.com>
2023-08-03 21:55:28 +0700

    fix: title in landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
                     <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
                         <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="{{ asset('hero.png') }}">
                         <div class="text-center lg:w-2/3 w-full">
-                            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Gudang Ku</h1>
+                            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-gray-100">Peminjaman alat desa Tanjung Karang</h1>
                             <p class="mb-8 leading-relaxed">Tempat sewa barang dengan harga murah tapi kulitasnya tidak murahan.</p>
                             <div class="flex justify-center">
                                 @auth
